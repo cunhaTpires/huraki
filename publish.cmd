db8849cacc2abb90aa31dfc55c3491adeb281d1c
Cunha Thamires <35454986+cunhaTpires@users.noreply.github.com>
2018-01-21 18:07:21 -0200

    Check-in Project Huraki bot

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=harukibot-Web-Deploy.pubxml -p:Password=FlruC5PsmbDQKHWEbgMLLRPap3Bg9wcuqwliD15l7HCNxJxQYWBXXvhSgaiQ
+
